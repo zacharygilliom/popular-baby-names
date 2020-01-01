91bd93a475dade39b84dea3325f73ad9cd0bd375
zacharygilliom <zacharygilliom@gmail.com>
2020-01-01 17:34:12 -0500

    Changed the data to include the rank instead of count to better reflect the most popular names.  Added some better styling.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
+# Chose to use seaborn over matplotlib as it provides more customization
+sns.set(style='darkgrid')
 
 df = pd.read_csv("Popular_Baby_Names.csv")
 
 
 # some exploratory analysis to see how our data is organized
-print(df.columns)
-print(df.head())
+# print(df.columns)
+# print(df.head())
 
 # Noticed that some of the first names are all caps.
 # lets change it to standard typesetting with the first letter capitalized
@@ -16,11 +19,16 @@ df["Child's First Name"] = df["Child's First Name"].apply(lambda x: x.lower().ca
 print(df.head())	
 
 # want to create multiple scatter plots by each child's name
-for df in [i for i in df.groupby("Child's First Name") if i[1]['Count'].mean() > 150]:
-	plt.scatter(x=df[1]['Year of Birth'], y=df[1]['Count'], label=df[0])
+for df in [i for i in df.groupby("Child's First Name") if i[1]['Rank'].mean() < 11]:
+	sns.lineplot(x=df[1]['Year of Birth'], 
+			 	y=df[1]['Count'],
+			 	label=df[0],
+			 	err_style=None,
+			 	markers=True,
+			 	)
 	plt.legend(loc=1)
 
-plt.title('Occurences of baby names by birth year that are averaging greater than 175')
+plt.title('Rank of baby names by birth year that average in the top 10')
 plt.xlabel('Year of Birth')
 plt.ylabel('Occurences of name in each year')
 
